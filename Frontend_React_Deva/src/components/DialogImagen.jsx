fcabea7eaa6663ad1a94e17b0f0289bdc36aa86b
Frank8040 <frank.chunca@upeu.edu.pe>
2023-05-31 06:58:07 -0500

    Completado CRUD Catalogo

---
+++ Frontend_React_Deva/src/components/DialogImagen.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import { Dialog } from 'primereact/dialog';
+import { InputText } from 'primereact/inputtext';
+import { Dropdown } from 'primereact/dropdown';
+import { Button } from 'primereact/button';
+
+export const DialogCreateUpdate = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const { width, visible, header, footer, onHide, htmlFor_00, label_00, id_00, value_00, onChange_00, className_00, msgRequired_00, valueFile, imagen, onChangeFile } = props;
+
+  const fileInputRef = React.createRef();
+
+  const handleClick = () => {
+    fileInputRef.current.click();
+  };
+
+  return (
+    <Dialog
+      visible={visible}
+      style={{ width: width }}
+      breakpoints={{ "960px": "75vw", "641px": "90vw" }}
+      header={header}
+      modal
+      className="p-fluid"
+      footer={footer}
+      onHide={onHide}
+    >
+      <div className="field">
+        <label htmlFor={htmlFor_00} className="font-bold">
+          {label_00}
+        </label>
+        <InputText
+          id={id_00}
+          value={value_00}
+          onChange={onChange_00}
+          required
+          autoFocus
+          className={className_00}
+        />
+        {msgRequired_00}
+      </div>
+      <div className="field">
+        <div className="p-field">
+          <label htmlFor="file" className="font-bold">Imagen:</label>
+          <div className="p-inputgroup mt-3">
+            <input
+              ref={fileInputRef}
+              accept="image/*"
+              type="file"
+              className="p-inputtext"
+              style={{ display: "none" }}
+              onChange={onChangeFile}
+            />
+            <Button
+              type="button"
+              icon="pi pi-upload"
+              label="Seleccionar"
+              onClick={handleClick}
+            />
+            <InputText
+              readOnly
+              value={valueFile}
+              placeholder="Seleccionar archivo"
+            />
+          </div>
+          {imagen}
+        </div>
+      </div>
+    </Dialog>
+  );
+};
+
+export const DialogDelete = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const { visible, footer, onHide, msgDialogModal } = props;
+  return (
+    <Dialog
+      visible={visible}
+      style={{ width: "32rem" }}
+      breakpoints={{ "960px": "75vw", "641px": "90vw" }}
+      header={
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          Confirmar acción
+        </div>
+      }
+      modal
+      footer={footer}
+      onHide={onHide}
+    >
+      <div className="confirmation-content">
+        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', textAlign: 'center' }}>
+          <i className="pi pi-exclamation-triangle mb-3" style={{ fontSize: '4rem' }} />
+          {msgDialogModal}
+        </div>
+      </div>
+    </Dialog>
+  )
+}
---
+++ Frontend_React_Deva/src/services/ImagenService.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:9090/imagen";
+
+export const getImageList = () => {
+  return axios.get(API_URL);
+};
+
+export const createImage = (image) => {
+  return axios.post(API_URL, image);
+};
+
+export const updateImage = (id, image) => {
+  return axios.put(`${API_URL}/imagen/${id}`, image);
+};
+
+export const deleteImage = (id) => {
+  const url = `${API_URL}/${id}`;
+  return axios.delete(url);
+};
+
+export const deleteSelectedImages = (ids) => {
+  const deleteRequests = ids.map((id) => deleteImage(id));
+  return Promise.all(deleteRequests);
+};
